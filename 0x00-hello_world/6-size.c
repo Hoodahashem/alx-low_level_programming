0d72404011e2bd546dd5e2c466527b2df4dea522
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-15 19:10:40 -0700

    f*ck betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,6 @@
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)
-	Size of an int: 4 byte(s)
-	Size of a long int: 4 byte(s)
-	Size of a long long int: 8 byte(s)
-	Size of a float: 4 byte(s)");
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
 	return (0);
 }
