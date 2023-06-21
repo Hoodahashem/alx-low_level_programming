debc5ab78cb0cbc9fa96773e9b7fc516f3596e87
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-20 17:23:32 -0700

    next

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * void print_alphabet_x10(void) -  prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * return: Allways 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i=1; i<=10; i++)
+	{
+		print_alphabet()
+	}
+}
