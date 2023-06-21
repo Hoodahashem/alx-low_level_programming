cdb9054213f91652d3b062ab4ef74f10112c6904
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-21 07:20:09 -0700

    first

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * _isalpha - checks if there is and alphabet
+ *
+ * @c: the character
+ *
+ * Return: Returns 1 if c is a letter, lowercase or uppercase
+ * Returns 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
