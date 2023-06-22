6ade8e5ecfad0fc5475fa575eb19a63cfe92e92f
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-22 15:47:21 -0700

    first

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * mul(int a, int b) - function multiply the nums
+ *
+ * @a: an integer
+ * @b: the second intger
+ *
+ * Return: the value of the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
