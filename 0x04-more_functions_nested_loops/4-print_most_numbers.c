2d5e920b90edc40da879100b3a381c2c78f16b5e
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-22 16:39:27 -0700

    again 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,13 +11,9 @@ void print_most_numbers(void)
 
 	for (x = '0', x <= '9', x++)
 	{
-		if (x = '2' || x = '4')
+		if (!(x == '2' || x == '4'))
 		{
-			continue
-		}
-		else
-		{
-			putchar(x);
+			putchar(x)
 		}
 	}
 	putchar("\n");
