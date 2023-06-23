09ab050710ba27696ad44ca2403e6c511bf821fc
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-22 17:45:41 -0700

    finally the first

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+
+void print_line(int n)
+{
+	if (n >= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int x;
+
+		for (x = 1; x <= n; x++)
+		{
+			putchar('_');
+		}
+		putchar('\n')
+	}
+}
