3f64ea6f302e9738f004441331ce52beeed43d9c
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-22 17:12:52 -0700

    first

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			putchar(x)
+		}
+	}
+}
