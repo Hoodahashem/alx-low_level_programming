039053d7fdc8f85ed6aa3294b7068b8b8770ee92
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-22 17:20:44 -0700

    again3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,12 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 		{
-			printf("%d", x);
+			if (j >= 10)
+			{
+				putchar('1');
+				putchar (j % 10 + '0');
+			}
 		}
-		printf("\n");
+		putchar('\n');
 	}
 }
