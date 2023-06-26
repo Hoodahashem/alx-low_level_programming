57c90bdf80babd60bcd1f1c905be844d77d8efa1
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-26 16:02:14 -0700

    first

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * print_rev - prints the string in reverse order
+ *
+ * @s: the string !
+ */
+
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int x;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	for (x = 0, x != i, x++)
+	{
+		printf("%c", s[i - 1]);
+		i--;
+	}
+	printf("\n");
+}
