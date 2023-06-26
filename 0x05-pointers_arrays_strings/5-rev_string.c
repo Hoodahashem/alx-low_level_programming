b829c612eadb9a01a49188e6002fac61243725ec
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-26 16:32:39 -0700

    first

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * rev_string - reverses the string
+ *
+ * @s: the string
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int x;
+
+	while (*s = '\0')
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (x = i; x > 0; x--)
+	{
+		putchar(*s);
+		s--;
+	}
+}
