88a63e40be7c6ccfa76c6cd0f620153f8b5dc512
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-29 16:57:06 -0700

    first

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * _strncat - function that works such as strncat works
+ * @dest: first prameter
+ * @src: second prameter
+ * @n: the integer
+ * Return: the final product
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = *dest;
+	int i;
+
+	while (*ptr != '\0')
+	{
+		*ptr++;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		*ptr = *src;
+		*ptr++;
+		*src++;
+	}
+	return (dest);
+}
