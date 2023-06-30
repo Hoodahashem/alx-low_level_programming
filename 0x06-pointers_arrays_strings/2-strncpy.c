aaeae1a13a9df36d6ab1ed75eaf4fd0b402a9724
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-29 17:31:01 -0700

    first

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * _strncpy - function dose what strncpy dose
+ * @dest: first string
+ * @src: second string
+ * @n: the integer
+ * Return: final product
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *ptr = dest;
+
+	while (i < n && *src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		i++;
+	}
+	return (dest);
+}
