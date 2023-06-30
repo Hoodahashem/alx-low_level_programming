e22feff431bf0fa704469fb677523dc498e87d24
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-29 18:20:23 -0700

    first

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * reverse_array - function reverses an array
+ * @a: the array
+ * @n:the size
+ * Return:nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	for (; n >= 0; n--)
+	{
+		printf("%d", *a[n]);
+	}
+}
