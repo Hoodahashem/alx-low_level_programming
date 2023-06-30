eaf7d86c2e97813f21f17ec1baea990ee0fe0e12
Hoodahashem <mahmoudhashem335@gmail.com>
2023-06-29 18:02:40 -0700

    first

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * _strcmp - function dose what strcmp dose
+ * @s1:first string
+ * @s2:second string
+ * Return: the defferice
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		*s1++;
+		*s2++;
+	}
+	return (*s1 - *s2);
+}
