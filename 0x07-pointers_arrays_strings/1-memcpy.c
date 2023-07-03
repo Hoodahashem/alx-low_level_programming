e5c33847f45b6cfe13390c0b18c8efcf3f8c72cd
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-03 10:57:57 -0700

    again

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-
+/**
+ * _memcpy - copy the pointer into another pointer
+ * @dest:the first pointer
+ * @src:the second pointer
+ * @n:an integer
+ * Return:the final pointer
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
