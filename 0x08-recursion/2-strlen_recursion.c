a5c62a5d5301e9a5dc9103cb9c5efe77d8542c18
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-05 15:00:00 -0700

    on the process

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * _strlen_recursion - function return the len of string
+ * @s:the string
+ * Return:returns the len of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		i++;
+	}
+	_strlen_recursion(s + 1);
+	return (i);
+}
