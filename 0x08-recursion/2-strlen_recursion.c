3bf8c75b6b5b9c1bb2604b5a7d238d82d9275b11
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-05 15:58:49 -0700

    on the process

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int i;
 
 	if (*s == '\0')
 	{
 		return(0);
 	}
-	i++;
-	_strlen_recursion(s + 1);
-	return (i);
+	i = _strlen_recursion(s + 1);
+	return (i + 1);
 }
