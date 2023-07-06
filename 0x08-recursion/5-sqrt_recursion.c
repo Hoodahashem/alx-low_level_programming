27804ebb0161d8ddfb0f7271c15cd46b9ccd6ad9
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-05 17:39:39 -0700

    on the process

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt(0, n));
+	return (sqrt(0, n));
 }
 
 /**
@@ -28,5 +28,5 @@ int _sqrt(int n, int x)
 		return (-1);
 	else if (n * n == x)
 		return (n);
-	return (_sqrt(n + 1, x));
+	return (sqrt(n + 1, x));
 }
