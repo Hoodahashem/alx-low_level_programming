d62d7c04340ea9b18c5d8df18a64261867c77830
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-07 08:47:10 -0700

    on the process

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - is the main entery point
+ * @argc:an integer
+ * @argv:an array of strings
+ * Return:returns 0 if succeed
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0, i < argc, i++)
+	{
+		printf("%s", argv[i]);
+	}
+	return (0);
+}
