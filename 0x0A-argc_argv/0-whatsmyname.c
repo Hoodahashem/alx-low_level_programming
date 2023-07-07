d107763b97da47b45c8929ef7a21cde2982defc9
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-07 08:06:42 -0700

    on the process

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv:argument vector
  * Return: return 0 if succeed
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
