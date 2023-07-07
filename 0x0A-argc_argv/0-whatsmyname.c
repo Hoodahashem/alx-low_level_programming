ee8e3c1dce91003e5723a2140a628753ed962886
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-07 08:02:20 -0700

    on the process

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the main entery point
+ * @argc:argument count
+ * @argv:argument vector
+ * Return: return 0 if succeed
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[1]);
+	return (0);
+}
