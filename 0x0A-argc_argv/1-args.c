5ca0f120aea407d0649cb69c014c62de4187c438
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-07 08:35:24 -0700

    on the process

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv:an array of strings
  * Return:return 0 if succeed
  */
-int main(int argc, char __attribute__((__unused__)) *argv)
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d", argc);
 	return (0);
