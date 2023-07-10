69e1f455c6a2c8c13aba576ccbe014f3ea1c681f
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-10 04:39:23 -0700

    on the process

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,11 +8,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 	char *ptr = malloc(size * sizeof(char));
 
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
 	return (ptr);
 }
