c0b8f387eb715846cd6aa07bbfcb88214d6921bd
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-17 18:55:21 -0700

    I'm on the process

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
-#ifndef hello
-#define hello
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * struct dog - is struction in c
- * @name: is a member
- * @age: is another member
- * @owner: giss what its another member
- */
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
+
+/**
+*dog_t - Typedef for struct dog
+*/
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
 #endif
