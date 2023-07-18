90c52ef5e62d360fcac9dff97efe1dd606b1bfbf
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-17 18:04:00 -0700

    I'm on the process

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
-#include<stdio.h>
+#ifndef hello
+#define hello
+/**
+ * struct dog - is struction in c
+ * @name: is a member
+ * @age: is another member
+ * @owner: giss what its another member
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+#endif
