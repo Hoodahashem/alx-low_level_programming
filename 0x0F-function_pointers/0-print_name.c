d7df1fd9f95beadea83b02614d083022859571af
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-19 11:20:56 -0700

    on the process

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
 #include "function_pointers.h"
+
 /**
- * print_name - prints the name of the preson
- * @name:the name of the preson
- * Return: nothing
- * @f:the pointer
- */
+*print_name - prints a name
+*@name:name to print
+*@f: function which print name
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
-		f(name);
-	else
-		return;
+
+if (name == NULL || f == NULL)
+return;
+
+f(name);
+
 }
