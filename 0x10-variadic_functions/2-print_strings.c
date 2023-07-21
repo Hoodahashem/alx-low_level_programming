085102b09867cafd28ab03e26c1bdcc4ce751ec2
Hoodahashem <mahmoudhashem335@gmail.com>
2023-07-20 18:52:55 -0700

    on the process

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - normal function
+ * @separator:normal separator
+ * @n:integer
+ * Return:nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list args;
+
+	va_start(args, n);
+	for (x = 0; x < n; x++)
+	{
+		gustavo = va_arg(args, char *);
+
+		printf("%s", gustavo);
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		if (gustavo == NULL)
+		{
+			printf("nil");
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
