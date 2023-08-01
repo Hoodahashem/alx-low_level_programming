de61607ae6a3c69393aa0fd1d4f9d734f4fb2c81
Hoodahashem <mahmoudhashem335@gmail.com>
2023-08-01 05:55:11 -0700

    on the process

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - print len.
+ * print_listint - print len.
  * @h: list.
  * Return: The linked.
  */
 
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        num++;
-        h = h->next;
-    }
+	while (h)
+	{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+	}
 
-    return (num);
+	return (num);
 }
