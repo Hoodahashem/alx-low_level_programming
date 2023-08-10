132c34fe1b1ced7f545acf624e83d2f3cc742a95
Hoodahashem <mahmoudhashem335@gmail.com>
2023-08-10 09:43:01 -0700

    on the process

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * read_textfile - normal function
+ * @filename: param
+ * @letters: another one
+ * Return:the nums
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t ptr, lol1, lol2;
+	char *bluff;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	ptr = open(filename, O_RDONLY);
+
+	if (ptr == -1)
+	{
+		return (0);
+	}
+	bluff = malloc(sizeof(char) * letters);
+
+	lol1 = read(ptr, bluff, letters);
+	lol2 = write(1, bluff, letters);
+
+	free(bluff);
+	close(ptr);
+	return (lol2);
+}
