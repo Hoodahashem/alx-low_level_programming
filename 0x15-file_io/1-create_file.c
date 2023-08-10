9c648100bbf6f34d089a81e793899e5a5b9d7f6c
Hoodahashem <mahmoudhashem335@gmail.com>
2023-08-10 10:46:00 -0700

    on the process

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,10 +9,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int w, ptr, count = 0;
 
+	if (filename == NULL)
+		return (-1);
 	if (text_content != NULL)
 	{
-		text_content[count];
-		count++;
+		for (count = 0, text_content[count])
+			count++;
 	}
 	ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(ptr, text_content, count);
