c508e77d54c842fcc90c9fd263a289392858d5ff
Hoodahashem <mahmoudhashem335@gmail.com>
2023-08-31 12:08:49 -0500

    on the process

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int num = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
+
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		num = 2 * num + (b[i] - '0');
-		
+	}
+
 	return (num);
 }
+
