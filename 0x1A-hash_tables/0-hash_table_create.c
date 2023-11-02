7fa24e8bc0537a2f3908d22bb55eb1b64458dc43
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-03 01:37:24 +0200

    First Task!

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,29 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
+/**
+ * hash_table_create - create a new hash table
+ * @size: size
+ * Return: new hash table
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    
+	unsigned long int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
