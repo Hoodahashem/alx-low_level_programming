1075046225676cd045e12eb295a8a24793000fc9
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-03 02:47:31 +0200

    the 5th tash

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - Get the hash table
+ * @key: The key
+ * @ht: The hash table
+ * Return: The value
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int inky, i;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+	inky = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[inky] == NULL)
+	{
+		return (NULL);
+	}
+	for (i = inky; ht->array[i] != NULL; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			return (strdup(ht->array[i]->value));
+		}
+	}
+	return (NULL);
+}
