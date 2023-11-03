546c8c624f70b48cab23d7996500a2115e212125
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-03 02:28:21 +0200

    the 4th task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,16 +8,34 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int inky;
+	unsigned long int inky, i;
 	hash_node_t *new_node;
 
-	inky = key_index(key, ht->size);
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+	{
+		return (0);
+	}
+	inky = key_index(strdup(key), ht->size);
+	for (i = inky; ht->array[i] != NULL; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 	{
 		return (0);
 	}
-	new_node->key = key;
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
 	if (!new_node->value)
 	{
