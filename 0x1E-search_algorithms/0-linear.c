2e3bbdc7053185dfda38c4c0729fc6457499ea31
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-05 23:53:35 +0200

    statement added!

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,6 +17,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (; x < size; x++)
 	{
+		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 		if (array[x] == value)
 		{
 			return ((int)x);
